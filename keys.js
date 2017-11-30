d526441c4751672f45f8b5bb2a07c927658376a7
rdo <ryan@libertylearner.com>
2017-11-30 11:53:43 -0800

    implemented the bsaic argv inputs, and assembled commentary for rest of assignment

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'BXUGrcZ9upiaLQHCHb0uLhlEK',
+  consumer_secret: 'kPLxDnRJCgJT175Uf7gs47E4HrGDWEh7UBoJyijaLhR4hm1cSQ',
+  access_token_key: '936314018444091393-hU7uRBXFpWtX4bQANpD0Rhnl20ATkW4',
+  access_token_secret: 'L9aAURZ7DBejPqEAJfVoiy5u1GpgNTjmTwpbHpSYJrYBc',
 }
 
 module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+
+
+
+var argument1 = process.argv[2];
+var argument2 = process.argv[3];
+
+var randomTxt = 'random.txt'
+
+console.log(argument1 + argument2)
